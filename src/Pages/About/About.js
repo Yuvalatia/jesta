e3579fe9a21860fcbdb3f0ac0bef31a87400b0c6
YuvalAtia <yuvalatia1995@gmail.com>
2020-09-02 23:56:57 +0300

    first error commpoent and env

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,7 +2,15 @@ import React from "react";
 
 import "./About.css";
 const About = (props) => {
-  return <div className="about__container">im About</div>;
+  return (
+    <div className="about__container">
+      im About
+      <button onClick={() => console.log(process.env.REACT_APP_BACKEND_URL)}>
+        df
+      </button>
+      <p>cv{process.env.REACT_APP_BACKEND_URL}</p>
+    </div>
+  );
 };
 
 export default About;
---
+++ src/Shared/Error.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./Error.css";
+const Error = (props) => {
+  return <h1>fgfg</h1>;
+};
+
+export default Error;
