8dc6a11b6d664da524fb61933aa65164fc1f604e
YuvalAtia <yuvalatia1995@gmail.com>
2020-09-06 00:51:39 +0300

    register form validation added

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -9,6 +9,7 @@ import Container from "react-bootstrap/Container";
 import UserContext from "../../context/UserContext";
 // Hooks
 import useGlobalLoader from "../../hooks/global-loader";
+import useErrorHandler from "../../hooks/error-handler";
 // Services
 import { userLogin, userRegister } from "../../services/user.service";
 
@@ -17,6 +18,7 @@ const Auth = (props) => {
   const history = useHistory();
   const { userData, setUserData } = useContext(UserContext);
   const [loading, showLoader, hideLoader] = useGlobalLoader();
+  const [error, showError] = useErrorHandler();
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
@@ -49,7 +51,7 @@ const Auth = (props) => {
       hideLoader();
       history.push("/profile");
     } catch (err) {
-      console.log(err);
+      showError(err.response.data.message);
       hideLoader();
     }
   };
@@ -60,17 +62,18 @@ const Auth = (props) => {
 
   const RegisterFormSent = async (e) => {
     e.preventDefault();
-    /*     console.log(registerForm); */
-    showLoader();
-    try {
-      const response = await userRegister(registerForm);
-      localStorage.setItem("token", response.token);
-      setUserData({ token: response.token, user: response.user });
-      hideLoader();
-      history.push("/profile");
-    } catch (err) {
-      console.log(err);
-      hideLoader();
+    if (registerFormvalidation()) {
+      showLoader();
+      try {
+        const response = await userRegister(registerForm);
+        localStorage.setItem("token", response.token);
+        setUserData({ token: response.token, user: response.user });
+        hideLoader();
+        history.push("/profile");
+      } catch (err) {
+        showError(err.response.data.message);
+        hideLoader();
+      }
     }
   };
 
@@ -82,8 +85,51 @@ const Auth = (props) => {
     });
   };
 
+  const registerFormvalidation = () => {
+    // fullname check
+    if (!registerForm.fullname || registerForm.fullname.length < 4) {
+      showError("שם משתמש צריך להכיל לפחות 4 תווים");
+      return false;
+    }
+    // birth check
+    if (!registerForm.birth) {
+      showError("נא למלא תאריך לידה");
+      return false;
+    }
+    //email
+    if (!registerForm.email) {
+      showError("נא למלא אימייל");
+      return false;
+    }
+    // password check
+    if (
+      !registerForm.password ||
+      registerForm.password < 6 ||
+      registerForm.password !== registerForm.re_password
+    ) {
+      showError("סיסמאות צריכות להיות מעל 6 תווים וזהות");
+      return false;
+    }
+    // phone check
+    if (
+      !registerForm.phone ||
+      registerForm.phone.length !== 10 ||
+      isNaN(registerForm.phone)
+    ) {
+      showError("מספר פלאפון לצריך להכיל 10 מספרים");
+      return false;
+    }
+    // terms check
+    if (!registerForm.terms) {
+      showError("נא לאשר את התנאים");
+      return false;
+    }
+    return true;
+  };
+
   return (
     <React.Fragment>
+      {error}
       {loading}
       <div className="auth__container main_font">
         <Container>
